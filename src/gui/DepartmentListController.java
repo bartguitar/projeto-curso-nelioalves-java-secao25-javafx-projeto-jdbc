b4ced3519225553e5d4284121b0a86f68bb709fe
bartguitar <adrielbart@gmail.com>
2026-03-06 16:15:54 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,6 +1,8 @@
 package gui;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,8 +11,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
@@ -27,10 +31,16 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
 
+    private ObservableList<Department> obsList;
+
 
     @FXML
     public void onBtNewAction() {
         System.out.println("onBtNewAction");
+    }
+
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
     }
 
     @Override
@@ -38,11 +48,22 @@ public class DepartmentListController implements Initializable {
         initializeNodes();
     }
 
+    private DepartmentService service;
+
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 
         Stage  stage = (Stage) Main.getMainScene().getWindow();
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    public void updateTableView(){
+        if(service == null){
+            throw new IllegalArgumentException("Service was null");
+        }
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        tableViewDepartment.setItems(obsList);
     }
 }
